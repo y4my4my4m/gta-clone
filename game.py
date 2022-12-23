989e8f6ca164eaf87cba78c775bcead7b5c329b8
y4my4my4m <8145020+y4my4my4m@users.noreply.github.com>
2022-12-23 00:22:50 -0500

    NPC

--- game.py
+++ game.py
@@ -1,15 +1,19 @@
 from player import Player
 from vehicle import Vehicle
 from camera import Camera
+from npc import NPC
 class Game:
     def __init__(self):
         # Initialize game state variables
         self.player = Player(640,480)  # Create a player object
         self.enemies = []  # Create a list to store enemy objects
+        testNPC = NPC(450,120,2,self)
+        self.npcs = [testNPC]  # Create a list to store NPC objects
         self.items = []  # Create a list to store item objects
         self.score = 0  # Initialize the score
         self.vehicles = [Vehicle(230,480,5)]
         self.camera = Camera(0, 0, 4000, 4000)
+        self.enemies.append(testNPC)
 
     def update(self):      
         # Update the player's position and state
@@ -26,6 +30,10 @@ class Game:
         # Update the positions and states of vehicles
         for vehicle in self.vehicles:
             vehicle.update()
+
+        # Update the positions and states of NPCs
+        for npc in self.npcs:
+            npc.update()
 
         # Update the camera's position
         self.camera.update(self.player.x, self.player.y)
@@ -48,3 +56,7 @@ class Game:
         # Render vehicles
         for vehicle in self.vehicles:
             vehicle.render(screen, self.camera)
+
+        # Update the positions and states of NPCs
+        for npc in self.npcs:
+            npc.render(screen)
---
+++ npc.py
@@ -0,0 +1,52 @@
+import pygame
+import random
+import math
+
+class NPC:
+    def __init__(self, x, y, speed, game):
+        self.x = x  # Initial x position
+        self.y = y  # Initial y position
+        self.speed = speed # Initial speed
+        self.image = pygame.image.load("img/player.png")  # Load the NPC image
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.direction = random.randint(0, 360)  # Initial direction in degrees
+        self.speed = random.uniform(0.5, 1.5)  # Speed in pixels per frame
+        self.rotated_image = self.image
+        self.game = game
+        self.dead = False
+
+    def update(self):
+        self.hit_by_vehicle()  # Check if the NPC is being hit by a vehicle
+        # Check for collisions with vehicles
+        for vehicle in self.game.vehicles:
+            if self.rect.colliderect(vehicle.rect):
+                # NPC has been hit by a vehicle
+                self.hit_by_vehicle()
+        # Update the NPC's position based on their direction and speed
+        self.x += self.speed * math.cos(math.radians(self.direction))
+        self.y += self.speed * math.sin(math.radians(self.direction))
+        # Change the NPC's direction randomly
+        if random.random() < 0.01:
+            self.direction = random.randint(0, 360)
+        # Rotate the NPC image
+        self.rotated_image = pygame.transform.rotate(self.image, -self.direction)
+        # Calculate the center of the rotated image
+        center = self.rotated_image.get_rect().center
+        x = self.x - center[0]
+        y = self.y - center[1]
+        # Update the NPC's rect attribute to match the current x and y position
+        self.rect.move_ip(x - self.rect.x, y - self.rect.y)
+        # if self.dead:
+        #     self.game.enemies.remove(self)  # Remove the NPC from the game's enemies list
+
+    def render(self, screen):
+        if (self.dead == False):
+            # Draw the NPC on the screen
+            screen.blit(self.rotated_image, (self.x, self.y))
+
+    def hit_by_vehicle(self):
+        # Check if the NPC's rect is colliding with a vehicle's rect
+        for vehicle in self.game.vehicles:
+            if self.rect.colliderect(vehicle.rect):
+                self.dead = True  # Set the NPC's dead flag to True
+
